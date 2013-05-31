b3780cd78b020b9e8c66507dac78489772709a5e
Tim Tischler <tischler@purplecoffeecup.com>
2013-05-31 15:15:05 -0500

    Adding mongo + vim

---
+++ modules/people/manifests/tischler.pp
@@ -0,0 +1,7 @@
+class people::tischler { 
+	$home = "/Users/{$::luser}"
+	$working_dir = "${home}/working"
+	$dotfiles = "${working}/dotfiles"
+
+
+}
---
+++ modules/projects/manifests/homeaway.pp
@@ -0,0 +1,14 @@
+class projects::trollin {
+  include icu4c
+  include phantomjs
+
+  boxen::project { 'trollin':
+    dotenv        => true,
+    elasticsearch => true,
+    mysql         => true,
+    nginx         => true,
+    redis         => true,
+    ruby          => '1.9.3',
+    source        => 'boxen/trollin'
+  }
+}
